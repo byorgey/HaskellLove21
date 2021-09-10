a1b2a3487ecfffedd636118e5965098356eaefbb
Haisheng Wu mac16 <freizl@gmail.com>
2021-09-10 07:43:48 -0700

    solve autori

--- autori/Autori.hs
+++ autori/Autori.hs
@@ -1,2 +1,12 @@
 -- url: https://open.kattis.com/problems/autori
 
+import           Control.Arrow                  ( (>>>) )
+import           Data.Char                      ( isUpper )
+
+main :: IO ()
+main = interact $ lines >>> map solve >>> unlines
+
+solve :: String -> String
+solve [] = []
+solve (x : xs) | isUpper x = x : solve xs
+               | otherwise = solve xs
