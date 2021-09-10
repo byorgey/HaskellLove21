9a72eb7333a4a16ca94b5dfc21561d53233c9e79
Brent Yorgey <byorgey@gmail.com>
2021-09-09 23:47:28 -0500

    remove stopwatch again

--- stopwatch/Stopwatch.hs
+++
@@ -1,6 +0,0 @@
-import           Data.List.Split
-main=interact$solve.drop 1 . map read . words
-solve :: [Int]->String
-solve ts
-  |odd(length ts)="still runnin"
-  |otherwise=show.sum.map(\[a,b]->b-a).chunksOf 2$ts
